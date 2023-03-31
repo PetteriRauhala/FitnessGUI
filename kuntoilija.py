dcdfea2474d8854499c27cb84fecbbb4deaaaab5
PetteriRauhala <122267930+PetteriRauhala@users.noreply.github.com>
2023-03-31 14:42:44 +0300

    toimivasupersiistijuttu

---
+++ kuntoilija.py
@@ -0,0 +1,97 @@
+# KUNTOILIJAN TIEDOT OLIO-OHJELMOINTINA
+# =====================================
+
+# KIRJASTOT JA MODUULIT (LIBRARIES AND MODULES)
+# ---------------------------------------------
+
+import fitness
+
+# LUOKKAMÄÄRITYKSET (CLASS DEFINITIONS)
+# -------------------------------------
+
+# Kuntoilija-luokka Yliluokka JunioriKuntoilijalle (super class)
+
+
+class Kuntoilija:
+    """Luokka kuntoilijan tietoja varten"""
+
+    # Oliomudostin eli konstruktori, self -> tuleva olio
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli, paiva):
+
+        # Määritellään tulevan olion ominaisuudet (property) eli luokan kentät (field)
+        self.nimi = nimi
+        self.pituus = pituus
+        self.paino = paino
+        self.ika = ika
+        self.sukupuoli = sukupuoli
+        self.bmi = fitness.laske_bmi(self.paino, self.pituus)
+        self.punnitus_paiva = paiva
+
+    # Metodi rasvaprosentin laskemiseen (yleinen / aikuinen)
+    def rasvaprosentti(self):
+        self.rasvaprosentti = fitness.aikuisen_rasvaprosentti(
+            self.bmi, self.ika, self.sukupuoli)
+        return self.rasvaprosentti
+
+    # Metodit rasvaprosenttien laskemiseen USA:n armeijan metodeilla
+    def usa_rasvaprosentti_mies(self, pituus, vyotaro_ymparys, kaulan_ymparys):
+        """Laskee kehon rasvaprosentin USA:n armeijan kaavalla
+
+        Args:
+            pituus (float): pituus (cm)
+            vyotaro_ymparys (float): vyötärön ympärysmitta (cm)
+            kaulan_ymparys (float): kaulan ympärysmitta (cm)
+
+        Returns:
+            float: rasvaprosentti
+        """
+        usa_rasvaprosentti = fitness.usarasvaprosentti_mies(
+            pituus, vyotaro_ymparys, kaulan_ymparys)
+        return usa_rasvaprosentti
+
+    def usa_rasvaprosentti_nainen(self, pituus, vyotaron_ymparys, lantion_ymparys, kaulan_ymparys):
+        """Laskee kehon rasvaprosentin USA:n armeijan kaavalla
+
+        Args:
+            pituus (float): pituus (cm)
+            vyotaron_ymparys (float): vyötärön ympärysmitta (cm)
+            lantion_ymparys (float): lantion ympärysmitta (cm)
+            kaulan_ymparys (float): kaulan ympärysmitta (cm)
+
+        Returns:
+            float: rasvaprosentti
+        """
+        usa_rasvaprosentti = fitness.usarasvaprosentti_nainen(
+            pituus, vyotaron_ymparys, lantion_ymparys, kaulan_ymparys)
+        return usa_rasvaprosentti
+# JunioriKuntoilija-luokka Kuntoilija-luokan aliluokka (subclass)
+
+
+class JunioriKuntoilija(Kuntoilija):
+    """Luokka nuoren kuntoilijan tiedoille"""
+
+    # Konstruktori
+    def __init__(self, nimi, pituus, paino, ika, sukupuoli):
+
+        # Määritellään periytyminen, mitä ominaisuuksia perii
+        super().__init__(nimi, pituus, paino, ika, sukupuoli)
+
+    # Metodi rasvaprosentin laskemiseen (ylikirjoittu lapsen metodilla)
+    def rasvaprosentti(self):
+        self.rasvaprosentti = fitness.lapsen_rasvaprosentti(
+            self.bmi, self.ika, self.sukupuoli)
+        return self.rasvaprosentti
+
+
+if __name__ == "__main__":
+
+    # Luodaan olio luokasta Kuntoilija
+    kuntoilija = Kuntoilija('Kalle Kuntoilija', 180, 80, 40, 1)
+    print(kuntoilija.nimi, 'painaa', kuntoilija.paino, 'kg')
+    # print('painoindeksi on ',kuntoilija.painoindeksi())
+    print('rasvaprosentti on', kuntoilija.rasvaprosentti())
+
+    juniorikuntoilija = JunioriKuntoilija('Aku', 171, 65, 16, 1)
+    print(juniorikuntoilija.nimi, 'painaa', juniorikuntoilija.paino, 'kg')
+    # print('painoindeksi on ',kuntoilija.painoindeksi())
+    print('rasvaprosentti on', juniorikuntoilija.rasvaprosentti())
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import sys
 from PyQt5 import QtCore # Core functionality of Qt
 from PyQt5 import QtWidgets # UI elements functionality 
 from PyQt5.uic import loadUi
+import kuntoilija
+import timetools
 
 # Class for the main window 
 class MainWindow(QtWidgets.QMainWindow):
@@ -20,12 +22,59 @@ class MainWindow(QtWidgets.QMainWindow):
         loadUi('main.ui', self)
 
         # Define UI Controls ie buttons and input fields
+        self.nameLE = self.nameLineEdit
+        self.birthDateE = self.birthDateEdit
+        self.genderCB = self.genderComboBox
+        self.weighingDateE = self.weighingDateEdit
+
+        # Set the weighing date to current ...
+        self.weighingDateE.setDate(QtCore.QDate.currentDate())
+        self.heihgtSB = self.heihgtSpinBox
+        self.weightSB = self.weightSpinBox
+        self.neckSB = self.neckSpinBox
+        self.waistSB = self.waistSpinBox
+        self.hipSB = self.hipSpinBox
+    
         self.calculatePB = self.calculatePushButton
         self.calculatePB.clicked.connect(self.calculateAll)
 
+        self.savePB = self.savePushButton
+        self.savePB.clicked.connect(self.saveData)
+
     # Define slots ie methods 
+
+    # Calculates BMI, Finnish and US fat percentages and updates corresponding labels
     def calculateAll(self):
-        self.bmiLabel.setText('100')
+        name = self.nameLE.text()
+        height = self.heihgtSB.value() # Spinbox value as an integer
+        weight = self.weightSB.value()
+
+        # Convert birthday to ISO string using QtCore's methods
+        birthday = self.birthDateE.date().toString(format=QtCore.Qt.ISODate)
+        
+        # Set Gender Value according to Combobox value
+        gendertext = self.genderCB.currentText()
+        if gendertext == 'Mies':
+            gender = 1
+        else:
+            gender = 0
+
+        # Convert Weighing data to ISO string 
+        dateOfWeighing = self.weighingDateE.date().toString(format=QtCore.Qt.ISODate)
+
+        # Calculate time difference using our home made tools
+        age = timetools.datediff2(birthday, dateOfWeighing, 'year')
+
+        # Create an athlete from Kuntoilija class
+        athlete = kuntoilija.Kuntoilija(name, height, weight, age, gender, dateOfWeighing)
+        bmi = athlete.bmi
+
+        self.bmiLabel.setText(str(bmi))
+
+    # Saves data to disk
+    def saveData(self):
+        pass
+
 
 if __name__ == "__main__":
     # Create the application
